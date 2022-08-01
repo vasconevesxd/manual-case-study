6d7252e9a7d87c1cac075bb3eabdc4ae7d295318
Vasco Neves <vasco.neves98@gmail.com>
2022-08-01 12:08:34 +0100

    fix header

--- components/base/Layout.js
+++ components/base/Layout.js
@@ -1,17 +1,11 @@
 import Navbar from '../base/Navbar'
 import Footer from '../base/Footer'
-import Meta from '../base/Meta'
-import Head from 'next/head'
-
 import { useRouter } from 'next/router'
 
 const Layout = ({children}) => {
     const router = useRouter().pathname
     return (
     <>
-        <Head>
-            <Meta/>
-        </Head>
         {router !== '/quizzes' ? <Navbar/> : null}
         {children}
         {router !== '/quizzes' ? <Footer/> : null}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import Jumbotron from '../components/elements/Jumbotron'
 import Steps from '../components/layouts/Steps'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Manual: Men's Healthcare. Made easy. More than a pharmacy.</title>
+        <meta name="description" content="We're here to help give you all the right info and choices. From erectile dysfunction to hair loss and beyond. So you can be the healthiest, happiest man you can be." />
+        <link rel="icon" href="/favicon.ico" />  
+      </Head>
       <main>
         <Jumbotron/>
         <Steps/> 
